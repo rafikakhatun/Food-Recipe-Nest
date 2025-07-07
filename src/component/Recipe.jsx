368bfb6e390ec907b485717acc8946126432c8cc
rafika khatun <rafikakhatun607@gmail.com>
2025-07-07 12:38:54 +0530

    Fetch and display dynamic card data from JSON file

--- src/component/Recipe.jsx
+++ src/component/Recipe.jsx
@@ -1,11 +1,45 @@
 import { faUtensils } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Foodcard from "./foodcard";
+import { useEffect, useState } from "react";
 
 function Recipe() {
+
+    // food json data usestate
+    const [foodData, setFoodData] = useState([]);
+
+
+    // food json data api call
+
+    const fetchJsonData = async () => {
+
+        const response = await fetch("/fooddata.json");
+
+        const result = await response.json();
+
+        setFoodData(result)
+
+
+
+
+
+    }
+
+
+
+
+
+    useEffect(() => {
+        fetchJsonData();
+    }, [])
+
+
+
+
     return (
         <>
             <section className="w-full py-14 px-12">
-                <div className="flex flex-col md:flex-row justify-center items-center text-center gap-4">
+                <div className="flex flex-col md:flex-row justify-center items-center text-center gap-8">
                     <h1 className="text-2xl font-semibold text-gray-700 italic">
                         Explore <span className="text-orange-500">100+</span> Delicious Recipes, All in One Place
                     </h1>
@@ -14,12 +48,17 @@ function Recipe() {
                         className="mt-4 md:mt-0 md:ml-4 text-orange-500 text-2xl"
                     />
                 </div>
-                <div>
-                    <div className="px-6 py-4  w-72 shadow border border-gray-300 rounded-md space-y-4">
-                        <h1 className="text-2xl text-center font-semibold text-gray-700 italic">Royal Biryani</h1>
-                        <img className="w-64 h-64 rounded-md transform transition duration-300 ease-in-out hover:scale-105 " src="src/assets/biriyani.jpeg" alt="food" />
-                        <button className="px-5 py-1 w-full rounded-xl bg-orange-400 hover:bg-orange-500 text-white cursor-pointer hover:underline">See Complete Recipe </button>
-                    </div>
+
+                <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 place-items-center gap-4 mt-16">
+                    {foodData.map((foodDatas, index) => (
+
+
+                        <Foodcard key={index} PropsData={foodDatas}></Foodcard>
+
+
+                    ))}
+
+
                 </div>
             </section>
 
