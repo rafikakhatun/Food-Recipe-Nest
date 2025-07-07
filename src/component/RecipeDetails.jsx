231c1de174d3e78e6d7330be7d2302334431faa4
rafika khatun <rafikakhatun607@gmail.com>
2025-07-07 13:42:07 +0530

    Add dynamic route setup for recipe details page using React Router

---
+++ src/component/RecipeDetails.jsx
@@ -0,0 +1,16 @@
+function RecipeDetails(){
+    return(
+
+
+
+
+
+        <>
+        
+        
+        
+        </>
+    )
+}
+
+export default RecipeDetails;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,35 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import RecipeDetails from './component/RecipeDetails.jsx'
+
+
+  const route = createBrowserRouter([
+
+  {
+    path:"/",
+    element:<App></App>
+
+  },
+  
+  {
+    path:"/recipe/:UserId",
+    element:<RecipeDetails></RecipeDetails>,
+    loader:()=> fetch("/fooddata.json")
+  }
+
+
+
+
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+<RouterProvider router={route}/>
+    
+    
   </StrictMode>,
 )
+
+
