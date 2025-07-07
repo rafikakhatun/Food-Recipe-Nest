1ad66b2a4f6bdd97fb48de8d54c0a99a26abd636
rafika khatun <rafikakhatun607@gmail.com>
2025-07-07 21:27:04 +0530

    Copy recipe images to public/assets for dynamic rendering from JSON

--- src/component/RecipeDetails.jsx
+++ src/component/RecipeDetails.jsx
@@ -1,27 +1,57 @@
 import { useLoaderData, useParams } from "react-router-dom";
 
-function RecipeDetails(){
 
-const {UserId} = useParams();
-console.log(typeof(UserId));
+function RecipeDetails() {
 
-const loader = useLoaderData();
+    const { UserId } = useParams();
+    console.log(typeof (UserId));
 
+    const loadData = useLoaderData();
 
-    return(
-
-       
-
-         <>
-
-         <h1 className="text-5xl">hello</h1>
-
-         
+    const detailsRecipe = loadData.find(item =>item.id === Number(UserId))
+    console.log(detailsRecipe)
 
 
-        
-        
-        
+    return (
+
+
+
+        <>
+
+            <section className="py-12 px-14 w-full">
+
+                <div>
+                    {/* title */}
+                    <h1 className="text-3xl font-semibold text-orange-500 text-center">{detailsRecipe?.title}</h1>
+
+                    <div className="text-center space-x-5 mt-6">
+                        <span className="text-lg text-gray-600">{detailsRecipe?.servings} servings</span>
+                        <span className="text-lg text-gray-600"> Prep Time : {detailsRecipe?.prepTime}</span>
+                        <span className="text-lg text-gray-600"> Cook Time : {detailsRecipe?.cookTime}</span>
+
+                        <span className="text-lg text-gray-600">Total time:{detailsRecipe?.totalTime}</span>
+                    </div>
+
+                    <div>
+                        <img className="w-96 h-96 rounded-lg shadow  object-center mt-10 " src={detailsRecipe?.image} alt="foodimg" />
+                        <div>
+
+                        </div>
+                    </div>
+
+                </div>
+
+
+
+            </section>
+
+
+
+
+
+
+
+
         </>
     )
 }
